bd751086bd854fd1e5d8d91eb294dd3eff42e17d
MUKILTEO\1722902 <aydenkern07@gmail.com>
2024-09-17 13:08:19 -0700

    Player and a testing ground ill probably wind up changing the code later

---
+++ Retro Remake/Assets/testPM.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class testPM : MonoBehaviour
+{
+    public int score, coins, time;                          // Ints represent WHOLE NUMBERS
+    public float speed, xPosition, yPosition;               // Floats rep DECIMAL NUMBERS
+    public string menuLabel;                                // Strings Rep WORDS/PHRASES/TEXT
+    public bool enemtyIsDead, isGrounded, isPlayerDead;     // Bools rep Values that are TRUE OR FALSE
+    public Vector2 currentMovement, platformPosition, playerPostition, rotation; // Vector2's rep 2D DECIMALS
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
