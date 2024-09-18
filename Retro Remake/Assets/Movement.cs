27103210d467aec50ebbabbdccc39b65fb9656dc
MUKILTEO\1722902 <aydenkern07@gmail.com>
2024-09-18 13:22:55 -0700

    fixed some jumping code I need to fix it for the other jump button but I alos fixed some physics

--- Retro Remake/Assets/Movement.cs
+++ Retro Remake/Assets/Movement.cs
@@ -1,29 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.RuleTile.TilingRuleOutput;
 
 public class Movement : MonoBehaviour
 {
-    public KeyCode left;
-    public KeyCode right;
-    public float speed;
-    public KeyCode up;
-    
+    public KeyCode left = KeyCode.A, right = KeyCode.D, up = KeyCode.W, down = KeyCode.S;
+    public float speed = 10, jumpheight = 15;
+    public KeyCode jump = KeyCode.W;
+    private Rigidbody2D _rb;
+    [Header("Raycast")]
+
+    [SerializeField] float dist; //raycast range
+    [SerializeField] float downRange = -0.75f; //offset
+
+    bool ground;
+
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody2D>();
+
+        dist = transform.localScale.y; //set dist
+    }
 
     // Update is called once per frame
     void Update()
     {
         if (Input.GetKey(left))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-speed, GetComponent<Rigidbody2D>().velocity.y);
+            _rb.velocity = Vector2.left * speed;
         }
         if (Input.GetKey(right))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(speed, GetComponent<Rigidbody2D>().velocity.y);
+            _rb.velocity = Vector2.right * speed;
         }
         if (Input.GetKey(up))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, speed);
+            _rb.velocity = Vector2.up * speed;
+        }
+        if (Input.GetKey(down))
+        {
+            _rb.velocity = Vector2.down * speed;
+        }
+
+        //orgin position * in the down dir downRange, direction, range
+        bool ray = Physics2D.Raycast(transform.position * (Vector2.down * downRange), Vector2.down, dist);
+
+        //print(ray);
+
+        if (ray && Input.GetKeyDown(jump))
+        {
+            _rb.velocity = Vector2.up * jumpheight;
+        }
+        if (Input.GetKeyUp(jump))
+        {
+            _rb.velocity = new(_rb.velocity.x, _rb.velocity.y / 2);
         }
     }
 }
--- Retro Remake/Assets/testPM.cs
+++ Retro Remake/Assets/testPM.cs
@@ -9,6 +9,8 @@ public class testPM : MonoBehaviour
     public string menuLabel;                                // Strings Rep WORDS/PHRASES/TEXT
     public bool enemtyIsDead, isGrounded, isPlayerDead;     // Bools rep Values that are TRUE OR FALSE
     public Vector2 currentMovement, platformPosition, playerPostition, rotation; // Vector2's rep 2D DECIMALS
+    private Rigidbody2D rb;
+
 
     // Start is called before the first frame update
     void Start()
